7b1a9c73b450de0fc1124ad90a5383217421898f
Matthijs Ates <m.b.ates@student.tudelft.nl>
2023-11-16 13:13:43 +0100

    strange error in old file, thus placed methods in a new one

--- MPS_initializations.py
+++ MPS_initializations.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-
-
-
-
 def initialize_halfstate(N, d, chi):
     """ Initializes the MPS into a product state of uniform eigenstates """
     Lambda_mat = np.zeros((N+1,chi))
@@ -55,17 +51,18 @@ def initialize_up_or_down(N, d, chi, up):
     locsize = np.minimum(d**arr, chi)
     return Gamma_mat, Lambda_mat, locsize
 
-
-def initialize_LU_RD(N, d, chi):
-    """ Initializes the MPS linearly from completely up at the leftmost site to completely down at the rightmost site """
+def initialize_LU_RD(N, d, chi, scale_factor):
+    """ Initializes the MPS linearly from up at the leftmost site to down at the rightmost site """
+    """ scale_factor is a variable that defines the peak up/down values taken """
     Lambda_mat = np.zeros((N+1,chi))
     Gamma_mat = np.zeros((N,d,chi,chi), dtype=complex)
     
     Lambda_mat[:,0] = 1
     
-    temp = np.arange(N)/N
-    Gamma_mat[:,0,0,0] = np.sqrt(1-temp)
-    Gamma_mat[:,d-1,0,0] = np.sqrt(temp)
+    #temp = np.arange(N)/N * scale_factor
+    temp1 = np.arange(N)/N * scale_factor
+    Gamma_mat[:,0,0,0] = np.sqrt(temp1)
+    Gamma_mat[:,d-1,0,0] = np.sqrt(temp2)
     
     arr = np.arange(0,N+1)
     arr = np.minimum(arr, N-arr)
@@ -74,8 +71,20 @@ def initialize_LU_RD(N, d, chi):
     return Gamma_mat, Lambda_mat, locsize
     
     
+L = 11
+f = 0.9
+
+a = np.arange(L)/(L-1)*f
+b = 1-a
 
 
+#b = np.arange(L-1,-1,-1)/(L-1) * f
+
+print("test")
+print(b)
+print(a)
+
+print(b+a)
 
 
 
